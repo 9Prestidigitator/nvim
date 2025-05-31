ef40eb30d2895d6987024441ac3315ae48309b20
9prestidigitator <9prestidigitator@gmail.com>
2025-05-31 19:49:56 -0400

    tex rendering

--- lua/configs/vimtex.lua
+++ lua/configs/vimtex.lua
@@ -4,6 +4,6 @@ vim.opt.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_view_forward_search_on_start = false
 vim.g.vimtex_compiler_latexmk = {
-  aux_dir = "~/.cache/.texfiles/",
-  out_dir = "~/.cache/tex/"
+  aux_dir = vim.fn.expand("~/.cache/.texfiles/"),
+  out_dir = vim.fn.expand("~/.cache/tex/")
 }

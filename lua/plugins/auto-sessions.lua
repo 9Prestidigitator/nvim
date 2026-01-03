0fa697927a17d265ec9da98d5448788a69882e33
9Prestidigitator <9Prestidigitator@gmail.com>
2026-01-03 01:26:05 -0500

    better notes experience

--- lua/plugins/auto-sessions.lua
+++ lua/plugins/auto-sessions.lua
@@ -3,6 +3,7 @@ require("auto-session").setup({
 		"~/",
 		"~/Documents",
 		"~/Downloads",
+        "~/notes",
 	},
 })
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"

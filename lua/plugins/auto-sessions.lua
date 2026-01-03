ea6383c0b6f859cd919e5f4cec5aeb7799730b8f
9Prestidigitator <9Prestidigitator@gmail.com>
2026-01-03 01:36:50 -0500

    nevermind

--- lua/plugins/auto-sessions.lua
+++ lua/plugins/auto-sessions.lua
@@ -3,7 +3,6 @@ require("auto-session").setup({
 		"~/",
 		"~/Documents",
 		"~/Downloads",
-        "~/notes",
 	},
 })
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
